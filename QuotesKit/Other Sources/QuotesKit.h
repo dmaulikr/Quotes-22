069e7d7c07a87702fbfe0b6c0473e1eea2138860
Julian Grosshauser <julian@juliangrosshauser.com>
2015-05-10 00:09:24 +0200

    Add QuotesKit

---
+++ QuotesKit/Other Sources/QuotesKit.h
@@ -0,0 +1,15 @@
+//
+//  QuotesKit.h
+//  QuotesKit
+//
+//  Created by Julian Grosshauser on 10/05/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuotesKit.
+FOUNDATION_EXPORT double QuotesKitVersionNumber;
+
+//! Project version string for QuotesKit.
+FOUNDATION_EXPORT const unsigned char QuotesKitVersionString[];

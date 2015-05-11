b43a56423b79a00a980f50fa57d04503506b48d1
Julian Grosshauser <julian@juliangrosshauser.com>
2015-05-11 17:15:18 +0200

    Add QuotesViewController

---
+++ Quotes/Classes/View Controllers/QuotesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuotesViewController.swift
+//  Quotes
+//
+//  Created by Julian Grosshauser on 11/05/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class QuotesViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

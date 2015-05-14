d96f45fb0b9c7ad6a1a6fb1996a8c126ce43020e
Julian Grosshauser <julian@juliangrosshauser.com>
2015-05-14 23:35:15 +0200

    Connect to database

--- Backend/app.rb
+++ Backend/app.rb
@@ -2,8 +2,13 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'sinatra/base'
+require 'active_record'
 
 module Quotes
   class App < Sinatra::Base
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: 'quotes.db'
+    )
   end
 end

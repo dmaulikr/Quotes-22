4e0de0ab4927ebf70deb48a97a875b2ec2e066b9
Julian Grosshauser <julian@juliangrosshauser.com>
2015-05-14 23:37:26 +0200

    Use `Quotes` route

--- Backend/app.rb
+++ Backend/app.rb
@@ -4,11 +4,15 @@ require 'bundler/setup'
 require 'sinatra/base'
 require 'active_record'
 
+require_relative 'routes/quotes'
+
 module Quotes
   class App < Sinatra::Base
     ActiveRecord::Base.establish_connection(
       adapter: 'sqlite3',
       database: 'quotes.db'
     )
+
+    use Routes::Quotes
   end
 end

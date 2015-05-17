c1cf321621e13719523c678a83babe0f2de6d109
Julian Grosshauser <julian@juliangrosshauser.com>
2015-05-17 22:08:39 +0200

    Add `Quote` model

---
+++ QuotesKit/Classes/Models/Quote.swift
@@ -0,0 +1,15 @@
+//
+//  Quote.swift
+//  QuotesKit
+//
+//  Created by Julian Grosshauser on 17/05/15.
+//  Copyright (c) 2015 Julian Grosshauser. All rights reserved.
+//
+
+import RealmSwift
+
+public class Quote: Object {
+
+    dynamic var quote = ""
+    dynamic var author = ""
+}
